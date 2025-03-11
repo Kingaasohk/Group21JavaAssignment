930fbd66c687f6d7c4c616d200649969a7574161
aaron <kingaasohk@gmail.com>
2025-03-11 19:18:13 -0400

    Database and other adjustments

--- src/main/java/com/example/groupproject/database/database.java
+++ src/main/java/com/example/groupproject/database/database.java
@@ -1,7 +1,14 @@
 package com.example.groupproject.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+
+
 public class database {
 
-
-
+    private static final String URL = "jdbc:sqlite:pharmacy_stock.db";
 }
