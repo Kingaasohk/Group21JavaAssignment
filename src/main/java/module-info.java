3be99eddef9ca58c5a2bea89d0dcd809c11fd306
Aaron <kingaasohk@gmail.com>
2025-03-22 20:12:46 -0400

    purge for new build

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.groupproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.groupproject to javafx.fxml;
-    exports com.example.groupproject;
-}
